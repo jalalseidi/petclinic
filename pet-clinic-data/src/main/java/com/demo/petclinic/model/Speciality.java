70ff5f9bd2daa73d681b38516d91b61eb6cd5a8c
jalalSeidi <jalalseidi7@gmail.com>
2022-07-13 21:49:01 +0200

    add the speciality pojo for vet

---
+++ pet-clinic-data/src/main/java/com/demo/petclinic/model/Speciality.java
@@ -0,0 +1,14 @@
+package com.demo.petclinic.model;
+
+public class Speciality extends BaseEntitiy{
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
