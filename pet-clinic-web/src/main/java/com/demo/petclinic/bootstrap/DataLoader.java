2a063c76fb251d5c287e96c0626f1577df2801d9
jalalSeidi <jalalseidi7@gmail.com>
2022-07-16 23:35:28 +0200

    add petTypes to bootstrap

--- pet-clinic-web/src/main/java/com/demo/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/demo/petclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package com.demo.petclinic.bootstrap;
 
 import com.demo.petclinic.model.Owner;
+import com.demo.petclinic.model.PetType;
 import com.demo.petclinic.model.Vet;
 import com.demo.petclinic.services.OwnerService;
+import com.demo.petclinic.services.PetTypeService;
 import com.demo.petclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,15 +14,25 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
 
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType savedCatType = petTypeService.save(cat);
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
