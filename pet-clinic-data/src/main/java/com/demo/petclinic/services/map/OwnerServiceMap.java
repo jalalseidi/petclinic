3c617eb28950f4fe43c8ed04043711acf3437fd4
jalalSeidi <jalalseidi7@gmail.com>
2022-07-17 23:38:58 +0200

    enhance owners with pets and contact info

--- pet-clinic-data/src/main/java/com/demo/petclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/com/demo/petclinic/services/map/OwnerServiceMap.java
@@ -1,14 +1,23 @@
 package com.demo.petclinic.services.map;
 
 import com.demo.petclinic.model.Owner;
-import com.demo.petclinic.services.CrudService;
+import com.demo.petclinic.model.Pet;
 import com.demo.petclinic.services.OwnerService;
+import com.demo.petclinic.services.PetService;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
 public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
+
+    private final PetTypeService petTypeService;
+    private final PetService petService;
+
+    public OwnerServiceMap(PetTypeService petServiceMap, PetService petService) {
+        this.petTypeService = petServiceMap;
+        this.petService = petService;
+    }
 
     @Override
     public Set<Owner> findAll() {
@@ -22,8 +31,29 @@ public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements 
 
     @Override
     public Owner save(Owner object) {
-        return super.save(object);
+
+        if (object != null) {
+            if (object.getPets() != null) {
+                object.getPets().forEach(pet -> {
+                    if (pet.getPetType() != null) {
+                        if (pet.getPetType().getId() == null) {
+                            pet.setPetType(petTypeService.save(pet.getPetType()));
+                        }
+                    }else {
+                        throw new RuntimeException("Pet Type is Required");
+                    }
+                    if (pet.getId() == null) {
+                        Pet savedPet = petService.save(pet);
+                        pet.setId(savedPet.getId());
+                    }
+                });
+            }
+            return super.save(object);
+        } else {
+            return null;
+        }
     }
+
 
     @Override
     public void delete(Owner object) {
--- pet-clinic-data/src/main/java/com/demo/petclinic/services/map/PetTypeService.java
+++ pet-clinic-data/src/main/java/com/demo/petclinic/services/map/PetTypeService.java
@@ -1,14 +1,13 @@
 package com.demo.petclinic.services.map;
 
 import com.demo.petclinic.model.Pet;
-import com.demo.petclinic.services.CrudService;
 import com.demo.petclinic.services.PetService;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
-public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetService {
+public class PetTypeService extends AbstractMapService<Pet, Long> implements PetService {
     @Override
     public Set<Pet> findAll() {
         return super.findAll();
