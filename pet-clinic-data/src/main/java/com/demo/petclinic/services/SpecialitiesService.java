1ec8ea2a273a7eb4389a8193a57391c452f2824f
jalalSeidi <jalalseidi7@gmail.com>
2022-07-18 22:45:31 +0200

    create specialty map service

---
+++ pet-clinic-data/src/main/java/com/demo/petclinic/services/SpecialitiesService.java
@@ -0,0 +1,32 @@
+package com.demo.petclinic.services;
+
+import com.demo.petclinic.model.Speciality;
+
+import java.util.Set;
+
+public class SpecialitiesService implements CrudService<Speciality, Long> {
+    @Override
+    public Set<Speciality> findAll() {
+        return null;
+    }
+
+    @Override
+    public Speciality findById(Long aLong) {
+        return null;
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return null;
+    }
+
+    @Override
+    public void delete(Speciality object) {
+
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+
+    }
+}
