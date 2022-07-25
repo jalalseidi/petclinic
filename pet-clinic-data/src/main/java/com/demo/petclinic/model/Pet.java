a4be253eae0c618f12ae1a9673439ad0d81269dc
jalalSeidi <jalalseidi7@gmail.com>
2022-07-25 21:18:36 +0200

    create visit entity in JPA

--- pet-clinic-data/src/main/java/com/demo/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/demo/petclinic/model/Pet.java
@@ -2,6 +2,9 @@ package com.demo.petclinic.model;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
 @Entity
 @Table(name = "pets")
 public class Pet extends BaseEntitiy{
@@ -15,6 +18,9 @@ public class Pet extends BaseEntitiy{
     private Owner owner;
     @Column(name = "birth_date")
     private LocalDate birthdate;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
 
     public String getName() {
         return name;
