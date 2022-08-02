b5ce429d6c3c44d6cc08a65af93c00deb2b02e38
jalalSeidi <jalalseidi7@gmail.com>
2022-08-02 21:35:45 +0200

    add spring data JPA speciality service

---
+++ pet-clinic-data/src/main/java/com/demo/petclinic/services/map/VetMapService.java
@@ -0,0 +1,56 @@
+package com.demo.petclinic.services.map;
+
+import com.demo.petclinic.model.Speciality;
+import com.demo.petclinic.model.Vet;
+import com.demo.petclinic.services.CrudService;
+import com.demo.petclinic.services.SpecialtyService;
+import com.demo.petclinic.services.VetService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
+
+    private final SpecialtyService specialtyService;
+
+    public VetServiceMap(SpecialtyService specialtyService) {
+        this.specialtyService = specialtyService;
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+
+        if (object.getSpecialities().size() > 0) {
+            object.getSpecialities().forEach(speciality -> {
+                if (speciality.getId() == null) {
+                    Speciality savedSpecialty = specialtyService.save(speciality);
+                    speciality.setId(savedSpecialty.getId());
+                }
+            });
+        }
+
+
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
