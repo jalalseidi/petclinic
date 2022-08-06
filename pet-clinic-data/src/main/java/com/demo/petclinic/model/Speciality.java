9ecef71df50638da6798618b5b4ab68870b64727
jalalSeidi <jalalseidi7@gmail.com>
2022-08-06 23:44:55 +0200

    add project lombok to petclinic project

--- pet-clinic-data/src/main/java/com/demo/petclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/com/demo/petclinic/model/Speciality.java
@@ -1,19 +1,19 @@
 package com.demo.petclinic.model;
 
+import lombok.*;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 public class Speciality extends BaseEntitiy{
 
     @Column(name = "description")
     private String description;
 
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
--- pet-clinic-data/src/main/java/com/demo/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/demo/petclinic/model/Vet.java
@@ -1,11 +1,17 @@
 package com.demo.petclinic.model;
 
+import lombok.*;
 import org.hibernate.annotations.Fetch;
 
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "vets")
 public class Vet extends Person{
@@ -14,12 +20,4 @@ public class Vet extends Person{
     @JoinTable(name = "vet_specialties", joinColumns = @JoinColumn(name = "vet_id"),
     inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Speciality> specialities = new HashSet<>();
-
-    public Set<Speciality> getSpecialities() {
-        return specialities;
-    }
-
-    public void setSpecialities(Set<Speciality> specialities) {
-        this.specialities = specialities;
-    }
 }
