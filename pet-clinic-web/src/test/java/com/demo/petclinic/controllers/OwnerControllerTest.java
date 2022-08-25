762a2a3760aaeeca92deaf5b0d1ccad3d8a18647
jalalSeidi <jalalseidi7@gmail.com>
2022-08-26 00:12:17 +0200

    in addition to to testing the owner controller

--- pet-clinic-web/src/test/java/com/demo/petclinic/controllers/OwnerControllerTest.java
+++ pet-clinic-web/src/test/java/com/demo/petclinic/controllers/OwnerControllerTest.java
@@ -14,7 +14,13 @@ import org.springframework.test.web.servlet.MockMvcBuilder;
 import java.util.HashSet;
 import java.util.Set;
 
+import static org.hamcrest.Matchers.hasSize;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 @ExtendWith(MockitoExtension.class)
 class OwnerControllerTest {
 
@@ -39,10 +45,32 @@ class OwnerControllerTest {
     }
 
     @Test
-    void listOwners() {
+    void listOwners() throws Exception {
+        when(ownerService.findAll()).thenReturn(owners);
+
+        mockMvc.perform(get("/owners"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/index"))
+                .andExpect(model().attribute("owners", hasSize(2)));
+    }
+
+    @Test
+    void listOwnersByIndex() throws Exception {
+        when(ownerService.findAll()).thenReturn(owners);
+
+        mockMvc.perform(get("/owners/index"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/index"))
+                .andExpect(model().attribute("owners", hasSize(2)));
     }
 
     @Test
     void findOwners() {
+
+        mockMvc.perform(get("/owners/find"))
+                .andExpect((status().isOk()))
+                .andExpect(view().name("notimplemented"));
+
+        verifyNoInteractions(ownerService);
     }
 }
